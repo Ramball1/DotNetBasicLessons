8bda6aee8df3be85e3e1ad3a91a8df9758119abc
Ramball1 <ramball0981@gmail.com>
2024-05-20 21:13:02 +0200

    Update

---
+++ Homework_2/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("First digit: ");
+var number1 = Convert.ToInt32(Console.ReadLine());
+int num1 = number1 * 1000;
+Console.Write("Second digit: ");
+var number2 = Convert.ToInt32(Console.ReadLine());
+int num2 = number2 * 100;
+Console.Write("Third digit: ");
+var number3 = Convert.ToInt32(Console.ReadLine());
+int num3 = number3 * 10;
+Console.Write("Fourth digit: ");
+var number4 = Convert.ToInt32(Console.ReadLine());
+int num4 = number4;
+Console.WriteLine($"\tYour number: {num1 + num2 + num3 + num4}");
+
+
+Console.Write("\nFirst digit: ");
+var number5 = Convert.ToInt32(Console.ReadLine());
+int num5 = number5;
+Console.Write("Second digit: ");
+var number6 = Convert.ToInt32(Console.ReadLine());
+int num6 = number6 * 10;
+Console.Write("Third digit: ");
+var number7 = Convert.ToInt32(Console.ReadLine());
+int num7 = number7 * 100;
+Console.Write("Fourth digit: ");
+var number8 = Convert.ToInt32(Console.ReadLine());
+int num8 = number8 * 1000;
+Console.Write("Fifth digit: ");
+var number9 = Convert.ToInt32(Console.ReadLine());
+int num9 = number9 * 10000;
+Console.Write("Sixth digit: ");
+var number10 = Convert.ToInt32(Console.ReadLine());
+int num10 = number10 * 100000;
+Console.WriteLine($"\tYour number: {num5 + num6 + num7 + num8 + num9 + num10}");
+
+
+//Console.Write("\nFirst digit: ");
+//var number11 = Convert.ToInt32(Console.ReadLine());
+//int num11 = number11;
+//Console.Write("Second digit: ");
+//var number12 = Convert.ToInt32(Console.ReadLine());
+//int num12 = number12;
+//Console.Write("Third digit: ");
+//var number13 = Convert.ToInt32(Console.ReadLine());
+//int num13 = number13;
+//Console.WriteLine($"\tSum: {num11 + num12 + num13} \tArithmetical mean: {(num11 + num12 + num13) / 3}");
